ed68038c28fa9c0bd5b8786a3baaef5197f86663
garamv20 <garamv20@gmail.com>
2024-01-08 13:33:43 +0900

    crud

---
+++ src/test/java/zerobase/weather/JpaMemoRepositoryTest.java
@@ -0,0 +1,43 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+        //when
+        jpaMemoRepository.save(newMemo);
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+    }
+
+    @Test
+    void findByIdTest() {
+        //given
+        Memo newMemo = new Memo(11, "jpa");  // key값이 DB에서 자동생성되기 때문에 id가 11로 생성되지 않는다
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), "jpa");
+    }
+}
